93d10ff35aa6038a139487bdbae2d8dabb31287c
Ricard-Rabasso <sepm6285@anticipa.com>
2022-11-10 19:29:46 +0100

    added funciontality to add child routes dinamically

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ListadoComponent } from './listado/listado.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ListadoComponent,
-    NotFoundComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/listado/listado-routing.module.ts
+++ src/app/listado/listado-routing.module.ts
@@ -1,15 +1,45 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Router, RouterModule, Routes } from '@angular/router';
 import { ListadoComponent } from './listado.component';
 
-const routes: Routes = [
+export const routes: Routes = [
   { path: '', component: ListadoComponent},
-  { path: ':provincia', component: ListadoComponent, data:{uid:'provincia'}},
-  { path: ':provincia/:ciudad', component: ListadoComponent, data:{uid:'provincia y ciudad'}}
+  { path: ':provincia', component: ListadoComponent, data:{uid:'provincia'}}
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class ListadoRoutingModule { }
+export class ListadoRoutingModule {
+
+  constructor( private router: Router){
+    // new Routes mock... this info can be served trought endpoint
+    const newRoutes = [
+      {
+        rootPath: 'listado',
+        routes: [
+          { path: ':provincia/:poblacion', component: ListadoComponent, data: { uid:'provincia y población'}}
+        ]
+      }
+    ];
+
+    setTimeout(()=>{
+      this.addDynamicChildRoutes( newRoutes );
+    });
+  }
+
+  addDynamicChildRoutes(newRoutes:any[]): void {
+    this.router.config.forEach((rootRoute: any) => {
+      newRoutes.forEach((newRoute: any) => {
+        if (rootRoute._loadedConfig && rootRoute.path === newRoute.rootPath) {
+          rootRoute._loadedConfig.routes = [
+            ...rootRoute._loadedConfig.routes,
+            ...newRoute.routes
+          ];
+        }
+      });
+    });
+  }
+
+}
